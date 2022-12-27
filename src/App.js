556a6a105f0418814669e5f7c8037526518d50e1
bd-dev-ismail <ismailratul7@gmail.com>
2022-12-28 00:11:39 +0600

    exploring github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Paper } from "@mui/material";
+import Navbar from "./Components/Navbar/Navbar";
+import Search from "./Components/Search/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper
+      sx={{
+        background: "#000",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Navbar />
+        <Search />
+      </Container>
+    </Paper>
   );
 }
 
---
+++ src/Components/Search/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div>
+            <p className='text-white'>Search</p>
+        </div>
+    );
+};
+
+export default Search;
